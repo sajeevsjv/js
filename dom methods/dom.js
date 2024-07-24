d299c1472578e1984484ffae91505480879a732d
sajeevsjv <sajeevsjv0@gmail.com>
2024-07-24 13:00:25 +0530

    get

--- dom methods/dom.js
+++ dom methods/dom.js
@@ -1,5 +1,18 @@
+//getElementById
 let a = document.getElementById('box');
 console.log(a);
-let content=a.textContent;
-console.log(content);
-a.textContent = "box one";
+let txt_content = a.textContent;
+console.log(txt_content);
+a.textContent = 'sajeeeeeeeeeeeev';
+
+//getElementByClass
+let b = document.getElementsByClassName("container");
+console.log(b);
+console.log(b[0].textContent);
+
+//getElementByTagName
+let c= document.getElementsByTagName('p');
+console.log(c);
+console.log(c[2].textContent);
+
+
