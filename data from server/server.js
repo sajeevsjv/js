92b42e03b2e9f342c54183095148142679022132
sajeev <sajeevsjv0@gmail.com>
2024-08-24 09:51:41 +0530

    sjv

---
+++ data from server/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const PORT = 4000;
+
+const server = http.createServer((req,res)=>{
+    const req_url = req.url;
+    console.log("req_url",req_url);
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    const parsed_url = url.parse(req_url);
+    if(parsed_url.pathname === "/json"){
+        res.writeHead(200,{'content-type':'text/json'});
+        res.end(fs.readFileSync('./user.json'));
+
+    }
+
+    else if (parsed_url.pathname === "/html") {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end(fs.readFileSync('./index.html'));
+    } 
+    else if (parsed_url.pathname === "/css") {
+        res.writeHead(200, { 'content-type': 'text/css' });
+        res.end(fs.readFileSync('./style.css'));
+    } 
+});
+
+server.listen(PORT, '127.0.0.1',()=>{
+    console.log(`server running at http://127.0.0.1:${PORT} ...`)
+})
+
+
+
+
+
+
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,56 @@
-const createserver = require ('http').createServer;
+const http = require('http');
+const PORT = 3000;
+const url = require('url');
+const fs = require('fs'); 
+const querystring = require('querystring');
 
-const server = createserver((req, res) =>{
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.end('hello world!\n');
+const server = http.createServer((req, res) => {
+    const req_url = req.url;
+    console.log("req_url", req_url);
+
+    const parsed_url = url.parse(req_url);
+    
+    if (parsed_url.pathname === '/') {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.end("hello world\n");  
+    }
+    else if (parsed_url.pathname === '/html') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(fs.readFileSync("./index.html"));
+    }
+    else if (parsed_url.pathname === '/style.css') {
+        res.writeHead(200, {'content-type': 'text/css'});
+        res.end(fs.readFileSync("./style.css"));
+    }
+    else if(parsed_url.pathname === "/submit" && req.method === 'POST'){
+        console.log("reached here ..");
+
+        let body ='';
+        req.on('data',(chunks)=>{
+            console.log("chunks:",chunks);
+            body += chunks.toString();
+        });
+
+        req.on('end',()=>{
+            let datas = querystring.parse(body);
+            console.log("datas :",datas);
+
+            console.log("name :",datas.name);
+            console.log("email :",datas.email);
+            console.log("Password :",datas.password);
+        });
+
+        res.writeHead(200, {'content-type':'text/plain'});
+        res.end('data send successfully ...');
+
+        
+    }
+    else {
+        res.writeHead(404, {'content-type': 'text/plain'});
+        res.end('404 Not Found');
+    }
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
-    console.log('listening on http://127.0.0.1:3000');
+server.listen(PORT, '127.0.0.1', () => {
+    console.log(`listening on http://127.0.0.1:${PORT}`);
 });
