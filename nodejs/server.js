3263d667772004bffaac94b73af7001d58211314
sajeev <sajeevsjv0@gmail.com>
2024-08-30 10:12:47 +0530

    sjv

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,18 +3,34 @@ const PORT = 3000;
 const url = require('url');
 const fs = require('fs'); 
 const querystring = require('querystring');
+const {MongoClient} = require('mongodb');
+const client = new MongoClient('mongodb://localhost:27017');
+
+async function connect(){
+    try{
+        await client.connect();
+        console.log("database connection established...");
+    }
+    catch(error){
+        console.log("error:",error);
+    }
+}
+connect();
+
 
 const server = http.createServer((req, res) => {
+    let db = client.db("college");
+    let collection = db.collection ("student");
     const req_url = req.url;
     console.log("req_url", req_url);
 
     const parsed_url = url.parse(req_url);
     
-    if (parsed_url.pathname === '/') {
+    if (parsed_url.pathname === '/hello') {
         res.writeHead(200, {'content-type': 'text/plain'});
         res.end("hello world\n");  
     }
-    else if (parsed_url.pathname === '/html') {
+    else if (parsed_url.pathname === '/') {
         res.writeHead(200, {'content-type': 'text/html'});
         res.end(fs.readFileSync("./index.html"));
     }
@@ -40,8 +56,25 @@ const server = http.createServer((req, res) => {
             console.log("Password :",datas.password);
         });
 
-        res.writeHead(200, {'content-type':'text/plain'});
-        res.end('data send successfully ...');
+       //save to a database
+       collection.insertOne({
+        name : datas.name,
+        email : datas.email,
+        password :datas.password
+       })
+
+       .then((message) =>{
+        console.log("message",message);
+        res.writeHead(201,{'content-Type':'text/plain'});
+        res.end("user created succesfully..");
+       })
+
+       .catch((error)=>{
+        console.log("error:",error);
+
+        res.writeHead(400,{'content-Type':'text/plain'});
+        res.end(error.message ? error.message : "user creation failed")
+       })
 
         
     }
